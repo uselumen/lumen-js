54fdc92290d97e74f102f71746b3e6595475a14a
John <bjohnoluwakeye@gmail.com>
2022-04-03 04:52:51 +0100

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
